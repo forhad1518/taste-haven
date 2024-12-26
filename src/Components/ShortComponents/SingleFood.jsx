48cfae3c3243c49e069da70e4fd4126b63e1196c
forhad1518 <ahforhad20@gmail.com>
2024-12-26 08:51:43 +0600

    0 purchase ignore

--- src/Components/ShortComponents/SingleFood.jsx
+++ src/Components/ShortComponents/SingleFood.jsx
@@ -98,25 +98,27 @@ const SingleFood = () => {
                     <Typography color="gray" className="mb-8 font-normal">
                         {food?.description}
                     </Typography>
-                    <Link to={`/purchase/${food?._id}`} className="inline-block">
-                        <Button variant="text" className="flex items-center gap-2">
-                            Purchase
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor"
-                                strokeWidth={2}
-                                className="h-4 w-4"
-                            >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
-                                />
-                            </svg>
-                        </Button>
-                    </Link>
+                    {
+                        food?.quantity === 0 ? <h1 className="text-red-600 font-bold">Sorry! he/she stock not is available</h1> : <Link to={`/purchase/${food?._id}`} className="inline-block">
+                            <Button variant="text" className="flex items-center gap-2">
+                                Purchase
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                    strokeWidth={2}
+                                    className="h-4 w-4"
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
+                                    />
+                                </svg>
+                            </Button>
+                        </Link>
+                    }
                 </CardBody>
             </Card>
         </div>
